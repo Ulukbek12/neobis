937b8533d92082a9198c90704321378e75a06d93
Ulukbek <tu12257@auca.kg>
2024-03-01 11:32:07 +0600

    PopulationIncrease second commit

--- neoProject/src/week1/PopulationIncrease.java
+++ neoProject/src/week1/PopulationIncrease.java
@@ -5,29 +5,26 @@ import java.util.Scanner;
 public class PopulationIncrease {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int attempt = 0;
         int lines = scanner.nextInt();
-        if(lines >= 1 && lines <=3000) {
-            do {
-                int A = scanner.nextInt();
-                int B = scanner.nextInt();
-                double A_growRate = scanner.nextDouble();
-                double B_growRate = scanner.nextDouble();
-                if((A >= 100 && A < 1000000 && A < B) && ((A_growRate >= 0.1 &&
-                    A_growRate <= 10.0) && (B_growRate >= 0.0 && B_growRate <= 10.0)
-                     && (A_growRate > B_growRate))){
-                    //100 150 1,0 0 --> 51 years
-                    while(A < B){
-                        A += (int) (A * A_growRate);
-                        attempt++;
-                        if(A == B){
-                            break;
-                        }
-                    }
+        for(int i = 0; i < lines; i++){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            double A_growRate = scanner.nextDouble();
+            double B_growRate = scanner.nextDouble();
+            int attempts = 0;
+            //100 150 1,0 0
+            while(A <= B){
+                A = A + (int)(A * A_growRate/100);
+                B = B + (int)(B* B_growRate/100);
+                attempts++;
+                if (attempts > 100) {
+                    System.out.println("Mais de 1 seculo.");
+                    break;
                 }
-                    lines--;
-            } while (lines > 0);
+            }
+            if (attempts <= 100) {
+                System.out.println(attempts + " anos.");
+            }
         }
-        System.out.println(attempt);
     }
 }
